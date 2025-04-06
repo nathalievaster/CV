60fc6063ab4c987fc9f2ceceb84610354e4aee93
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-04-06 21:05:31 +0200

    formulär + error för att lägga till kurs

--- server.js
+++ server.js
@@ -10,8 +10,38 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true}));
 
+// Visa alla kurser (Startsida)
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index"); // Renderar index.ejs
+});
+
+// Lägg till en ny kurs
+app.get("/add", (req, res) => {
+    res.render("add", {
+        error: ""
+    }); // Renderar add.ejs (lägg till kurs-formulär)
+});
+
+// Om sidan
+app.get("/about", (req, res) => {
+    res.render("about"); // Renderar about.ejs (om sidan)
+});
+
+app.post("/add", (req, res) => {
+    let coursecode = req.body.coursecode;
+    let coursename = req.body.coursename;
+    let syllabus = req.body.syllabus;
+    let progression = req.body.progression;
+    let error = "";
+
+    if (!coursecode || !coursename || !syllabus || !progression) {
+        error = "Du måste fylla i alla fält!";
+        res.render("add", { error: error }); // Rendera om "add" sidan och skicka med felet
+    } else {
+    }
+        res.render("add", {
+            error: error
+        });
 });
 
 app.listen(port, () => {
