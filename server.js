1d4aabb1c1f71b2f8d534496866cc9555babc257
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-04-06 20:37:16 +0200

    startade upp servern

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require("express");
-const bodyParse = require("body-parser");
+const bodyParser = require("body-parser");
 const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database("./db/courses.db");
+
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log("Application started on port: " + port);
+})
